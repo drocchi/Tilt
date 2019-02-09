284f3cfabc5627c1d8cf0552399994d9ee5252d7
drocchi <drocchi@mail.usf.edu>
2019-02-09 16:38:47 -0500

    code cleaning

---
+++ Experiment TILT/Assets/Scripts/TiltContolScripts/PhoneTilt.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneTilt : MonoBehaviour {
+
+	public Rigidbody rb;
+	public GameObject sphere;
+	public float ballSpeed;
+
+	void Start() {
+        rb = sphere.GetComponent<Rigidbody>();
+    }
+	void FixedUpdate () {
+
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+		rb.AddForce(transform.right * dir.x * ballSpeed);
+    }
+}
