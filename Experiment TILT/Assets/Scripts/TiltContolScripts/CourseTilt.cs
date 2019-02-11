df13a17d15e5e8ddb336b173612dd724fdbcf211
drocchi <drocchi@mail.usf.edu>
2019-02-11 16:23:26 -0500

    CourseTilt.cs cleaning

--- Experiment TILT/Assets/Scripts/TiltContolScripts/CourseTilt.cs
+++ Experiment TILT/Assets/Scripts/TiltContolScripts/CourseTilt.cs
@@ -4,38 +4,39 @@ using UnityEngine;
 
 public class CourseTilt : MonoBehaviour {
 
+	public Rigidbody rb;
+	private Vector3 slowDown;
 	public float torqueSpeed;
 	public float stopSpeed;
-	public Rigidbody rb;
-	public Vector3 torque;
-	private Vector3 slowDown;
 	
 	void Start() {
         rb = GetComponent<Rigidbody>();
     }
 
+	//comments hold angularVelocity set method
 	void FixedUpdate () {
-		Vector3 bob = new Vector3 (0,0,1);
+		Vector3 baseVel = new Vector3 (0,0,1);
 		//float turn = Input.GetAxis("Horizontal");
 		if (Input.GetKey("a")) {
-			//rb.AddTorque(transform.forward * torqueSpeed);
-			rb.angularVelocity = bob;
+			rb.AddTorque(transform.forward * torqueSpeed);
+			//rb.angularVelocity = baseVel;
 		}
 		else if (Input.GetKey("d")) {
-			//rb.AddTorque(-transform.forward * torqueSpeed);
-			rb.angularVelocity = -bob;
+			rb.AddTorque(-transform.forward * torqueSpeed);
+			//rb.angularVelocity = -baseVel;
 		}	
-		else{
-			if (rb.angularVelocity.z > 0){
-				slowDown = new Vector3 (0,0,rb.angularVelocity.z - stopSpeed);
-				rb.angularVelocity= slowDown;
-			if (rb.angularVelocity.z < 0){
-				slowDown = new Vector3 (0,0,rb.angularVelocity.z + stopSpeed);
-				rb.angularVelocity= slowDown;
-				}
-			//rb.angularVelocity = Vector3.zero;
-			}	
+		// else{
+		// 	if (rb.angularVelocity.z > 0){
+		// 		slowDown = new Vector3 (0,0,rb.angularVelocity.z - stopSpeed);
+		// 		rb.angularVelocity= slowDown;
+		// 	if (rb.angularVelocity.z < 0){
+		// 		slowDown = new Vector3 (0,0,rb.angularVelocity.z + stopSpeed);
+		// 		rb.angularVelocity= slowDown;
+		// 		}
+		// 	//rb.angularVelocity = Vector3.zero;
+		// 	}	
 		Debug.Log(rb.angularVelocity);
 		}
 	}
-}
+//}
+
